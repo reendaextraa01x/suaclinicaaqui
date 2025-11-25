f155f54ef55152499e0b18c53d540fb757dd092d
reendaextra.a extra <reendaextraa01@gmail.com>
2025-11-25 14:37:20 +0000

    o fundo esta preto, faça algo bonito

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -64,6 +64,7 @@ export function Hero() {
       )}
 
       <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/30 to-black/10" />
+      <div id="diamond-dust" />
 
       <motion.div
         className="pointer-events-none absolute inset-0 opacity-100 transition-opacity duration-300"
